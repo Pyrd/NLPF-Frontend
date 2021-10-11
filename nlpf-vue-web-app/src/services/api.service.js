7c5e6b64a366d3f702ab91215d6dfe3b486eebba
leo <l.hantsch@yahoo.fr>
2021-10-11 20:26:00 +0200

    Correction of the fetch user function

--- nlpf-vue-web-app/src/services/api.service.js
+++ nlpf-vue-web-app/src/services/api.service.js
@@ -1,10 +1,10 @@
 import axios from 'axios'
 
-const API = "https://stormy-taiga-31121.herokuapp.com"
+const URL_API = "https://stormy-taiga-31121.herokuapp.com"
 
 export const fetchUser = async () => {
     try {
-        const response = await axios.get('$(API)/user/me')
+        const response = await axios.get(`${URL_API}/user/me`)
         return response.data
     } catch (error) {
         console.error('An error happened: ${error}')
