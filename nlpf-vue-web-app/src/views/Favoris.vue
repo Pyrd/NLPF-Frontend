a0b75b32bc43561525caf21c3cbf038470cb8c42
Martin Coulon <martin.coulon@epita.fr>
2021-10-12 12:01:40 +0200

    Add a favorite page

--- nlpf-vue-web-app/src/views/Favoris.vue
+++ nlpf-vue-web-app/src/views/Favoris.vue
@@ -1,5 +1,77 @@
 <template>
-  <div>Favoris</div>
+<v-container>
+<v-row>
+ <v-card
+    class="mx-auto"
+    max-width="344"
+  >
+    <v-card-text>
+      <div>Favoris id</div>
+      <p class="text-h4 text--primary">
+        Favoris 1: Année PRIX
+      </p>
+      <p>Localisation</p>
+      <div class="text--primary">
+        Code postal:
+      </div>
+      <div class="text--primary">
+       Ville:
+      </div>
+      <div class="text--primary">
+       Adresse:
+      </div>
+      <div class="text--primary">
+       Number:
+      </div>
+    </v-card-text>
+    <v-card-actions>
+      <v-btn
+        text
+        color="teal accent-4"
+        @click="reveal = true"
+      >
+        Favoris
+      </v-btn>
+    </v-card-actions>
+  </v-card>
+</v-row>
+
+<v-row>
+  <v-card
+    class="mx-auto"
+    max-width="344"
+  >
+    <v-card-text>
+      <div>Favoris id</div>
+      <p class="text-h4 text--primary">
+        Favoris 2: Année PRIX
+      </p>
+      <p>Localisation</p>
+      <div class="text--primary">
+        Code postal:
+      </div>
+      <div class="text--primary">
+       Ville:
+      </div>
+      <div class="text--primary">
+       Adresse:
+      </div>
+      <div class="text--primary">
+       Number:
+      </div>
+    </v-card-text>
+    <v-card-actions>
+      <v-btn
+        text
+        color="teal accent-4"
+        @click="reveal = true"
+      >
+        Favoris
+      </v-btn>
+    </v-card-actions>
+  </v-card>
+</v-row>
+</v-container>
 </template>
 
 <script>
